0cf91346465993acb83e65f9ca90280714e1dc8d
Mateusz Pietrzak <pietrzakm002@gmail.com>
2023-08-20 02:15:01 +0200

    Changes in dekstop view

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
     fontFamily: {
       display: ["Montserrat", "sans-serif"],
     },
+
     extend: {
       colors: {
         "project-green": "#1B5B31",
         "project-beige": "#DCC1AB",
         "project-black": "#111111",
         "project-button-text": "#F5F0EC",
+      },
+      screens: {
+        "3xl": "1920px",
       },
     },
   },
